0f883e15f60f2007037b6b68f67d77e9e8c44808
lizard <shotup0101@gmail.com>
2021-09-20 22:03:19 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Main, Header } from "./styled";
+import React from "react";
+import Info from "./info_page/info";
+
+const HeaderContainer = () => {
+    return (
+        <Header>
+            <div>
+                <p className="Title">Lizard's Web</p>
+            </div>
+            <div>
+                <p className="Link">Home</p>
+            </div>
+        </Header>
+    );
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Main>
+            <HeaderContainer />
+            <Info />
+        </Main>
+    );
 }
 
 export default App;
---
+++ src/info_page/info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Main, Banner } from "./styled";
+
+const Info = () => {
+    return (
+        <Main>
+            <Banner>
+                <p>I'm Lizard</p>
+                <p>I like to code a lot</p>
+            </Banner>
+        </Main>
+    );
+};
+
+export default Info;
